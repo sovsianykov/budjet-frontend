7130e8e83595782b03a10ce404d6f037600c409f
sovsianykov <serhii.ovsianykov@dataart.com>
2026-02-27 14:56:13 +0200

    replace instead redirect

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,57 +1,170 @@
 "use client";
 
-import { useForm } from "react-hook-form";
-import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/navigation";
-import { Container, Paper, TextField, Button, Alert } from "@mui/material";
-import { useState } from "react";
+import { useAuth } from "@/hooks/useAuth";
+import { useForm, Controller } from "react-hook-form";
 
-type Form = { email: string; password: string };
+import {
+    Box,
+    Button,
+    TextField,
+    Typography,
+    Container,
+    Paper,
+} from "@mui/material";
 
-export default function LoginMUI() {
+type LoginFormInputs = {
+    email: string;
+    password: string;
+};
+
+export default function LoginPage() {
     const { login } = useAuth();
     const router = useRouter();
-    const [error, setError] = useState<string | null>(null);
 
-    const { register, handleSubmit, formState: { errors, isSubmitting } } =
-        useForm<Form>();
+    const {
+        handleSubmit,
+        control,
+        formState: { errors, isSubmitting },
+    } = useForm<LoginFormInputs>({
+        defaultValues: { email: "", password: "" },
+    });
 
-    const onSubmit = async (data: Form) => {
+    // const onSubmit = async (data: LoginFormInputs) => {
+    //     try {
+    //         await login(data.email, data.password);
+    //         router.push("/dashboard");
+    //     } catch (err: unknown) {
+    //         if (err instanceof Error) {
+    //             alert(JSON.stringify(err.name, err.stack , err.message));
+    //         }
+    //     }
+    // };
+
+    const onSubmit = async (data: LoginFormInputs) => {
         try {
             await login(data.email, data.password);
+
+            // ждём один tick event loop,
+            // чтобы React применил setState внутри useAuth
+            await new Promise(resolve => setTimeout(resolve, 0));
+
             router.replace("/dashboard");
-        } catch (e) {
-            if (e instanceof Error) setError(e.message);
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                alert(
+                    JSON.stringify(
+                        {
+                            name: err.name,
+                            message: err.message,
+                            stack: err.stack,
+                        },
+                        null,
+                        2
+                    )
+                );
+            }
         }
     };
 
     return (
-        <Container maxWidth="xs">
-            <Paper sx={{ p: 4 }}>
-                {error && <Alert severity="error">{error}</Alert>}
+        <Container
+            maxWidth="xs"
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+                backgroundColor: "background.default",
+            }}
+        >
+            <Paper sx={{ p: 4, width: "100%", backgroundColor: "background.paper" }} >
+                <Typography
+                    variant="h4"
+                    fontWeight={700}
+                    align="center"
+                    mb={1}
+                >
+                    Sign In
+                </Typography>
 
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <TextField
-                        label="Email"
-                        fullWidth
-                        margin="normal"
-                        {...register("email", { required: true })}
-                        error={!!errors.email}
+                <Typography
+                    variant="body1"
+                    color="text.primary"
+                    align="center"
+                    mb={4}
+                >
+                    Welcome back to Family Budget site
+                </Typography>
+
+                <Typography variant="h5" mb={3} align="center">
+                    Login
+                </Typography>
+
+                <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} display="flex" flexDirection="column" gap={2}>
+                    {/* Email */}
+                    <Controller
+                        name="email"
+                        control={control}
+                        rules={{
+                            required: "Email обязателен",
+                            pattern: { value: /^\S+@\S+$/i, message: "Неверный email" },
+                        }}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                autoComplete="email"
+                                label="Email"
+                                type="email"
+                                fullWidth
+                                error={!!errors.email}
+                                helperText={errors.email?.message}
+                            />
+                        )}
                     />
 
-                    <TextField
-                        label="Password"
-                        type="password"
-                        fullWidth
-                        margin="normal"
-                        {...register("password", { required: true })}
-                        error={!!errors.password}
+                    {/* Password */}
+                    <Controller
+                        name="password"
+                        control={control}
+                        rules={{ required: "Password is required" }}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                autoComplete="current-password"
+                                label="Password"
+                                type="password"
+                                fullWidth
+                                error={!!errors.password}
+                                helperText={errors.password?.message}
+                            />
+                        )}
                     />
 
-                    <Button fullWidth type="submit" variant="contained" disabled={isSubmitting}>
-                        Login
+                    <Button type="submit" variant="contained" color="success" fullWidth disabled={isSubmitting}>
+                        {isSubmitting ? "Logging in..." : "Login"}
                     </Button>
-                </form>
+                </Box>
+                <Typography
+                    variant="body2"
+                    align="center"
+                    color="text.secondary"
+                    mt={3}
+                >
+                    Don&#39;t have an account? {" "}
+                    <Typography
+                        component="span"
+                        sx={{
+                            cursor: "pointer",
+                            fontWeight: 500,
+                            textDecoration: "underline",
+                        }}
+                        onClick={() => router.push("/register")}
+                    >
+                        sign up
+                    </Typography>
+                </Typography>
+
             </Paper>
         </Container>
     );
